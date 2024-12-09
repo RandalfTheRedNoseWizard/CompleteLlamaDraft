ea9f8026edd82b8b78448f2a854895272c4d8f94
nicomp42 <nicholdw@ucmail.uc.edu>
2024-12-08 19:03:46 -0500

    Fixed the key

--- CompleteLlamaDraft/mainPackage/main.py
+++ CompleteLlamaDraft/mainPackage/main.py
@@ -7,7 +7,7 @@ def main():
     json_file = 'dataPackage/EncryptedGroupHints Fall 2024 Section 001.json'
     english_file = 'dataPackage/UCEnglish.txt'
     encrypted_message = "gAAAAABnJ6xXVDc2AOy9IsNcfodS3viOIxRb_yskmNX2rLff-yUIeTxS-Hgv4H2tRTikS0xj0QUmIcc12aJZSxmtoPcXxabAaRKiSLWLcV6ATkPecfeag22bi4dLejPjXBitOOkeN9Rt5OIiT96z8kuxg9Z6CSFMQw=="
-    movie_key = "oSQSToOvQLs-_7SRDonuB1xB7K10RwCF7MdlQa1MCdc="
+    movie_key = "oSQSToOvQLs-_7SRDonuB1xB7K10RwCF7MdIQa1MCdc="
 
     # Decrypt the location
     location_finder = findLocation(json_file, english_file)
